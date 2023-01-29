fbae96759da13283c73a7f70b8f7ff0ff34c7ddf
yanglianoo <330070781@qq.com>
2023-01-29 18:33:00 +0800

    从实模式进入保护模式

--- src/loader.asm
+++ src/loader.asm
@@ -8,6 +8,7 @@ dw 0x55aa;魔数，用于判断错误
 mov si, loading
 call print
 xchg bx, bx
+;检测内存代码
 detect_memory:
     ;将ebx置为0
     xor ebx, ebx
@@ -35,21 +36,22 @@ detect_memory:
     cmp ebx, 0
     jnz .next
 
-    mov si, detecting
-    call print
-    xchg bx, bx
+    ; mov si, detecting
+    ; call print
+    ; xchg bx, bx
 
-    ;结构体数量
-    mov cx, [ards_count]
-    ;结构体指针
-    mov si, 0
-.show:
-    mov eax, [si + ards_buffer]
-    mov ebx, [si + ards_buffer + 8]
-    mov edx, [si + ards_buffer + 16]
-    add si, 20
-    xchg bx, bx
-    loop .show
+    jmp prepare_protect_mode
+;     ;结构体数量
+;     mov cx, [ards_count]
+;     ;结构体指针
+;     mov si, 0
+; .show:
+;     mov eax, [si + ards_buffer]
+;     mov ebx, [si + ards_buffer + 8]
+;     mov edx, [si + ards_buffer + 16]
+;     add si, 20
+;     xchg bx, bx
+;     loop .show
 ;阻塞
 jmp $
 
@@ -75,7 +77,71 @@ error:
     hlt;让cpu停止
     jmp $
     .msg db "Loading Error!!!", 10,13,0
+prepare_protect_mode:
+    cli ;关闭中断
+    ;打开A20线
+    in al, 0x92
+    or al, 0b10
+    out 0x92, al
 
-ards_count:
+    lgdt [gdt_ptr] ;加载GDT
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    
+    jmp word code_selector:protect_enable
+
+    ud2;出错
+[bits 32]
+protect_enable:
+    mov ax, data_selector
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov fs, ax
+    mov gs, ax;初始化数据段
+
+    mov esp, 0x10000
+
+    mov byte [0xb8000], 'P'
+
+    xchg bx, bx
+
+    mov byte [0x200000], 'P'
+
+    xchg bx, bx
+
+
+base equ 0
+limit equ 0xfffff
+
+code_selector equ (1 << 3)
+data_selector equ (2 << 3)
+gdt_ptr:
+    dw (gdt_end - gdt_base -1)
+    dd gdt_base
+gdt_base: 
+    dd 0,0
+gdt_code:
+    dw limit & 0xffff;
+    dw base  & 0xffff;
+    db (base >> 16) & 0xff;
+    db 0b1110 | 0b0001_0000
+    db 0b1100_0000 | (limit >> 16)
+    db (base >> 24) & 0xff
+gdt_data:
+    dw limit & 0xffff;
+    dw base  & 0xffff;
+    db (base >> 16) & 0xff;
+    db 0b0010 | 0b0001_0000
+    db 0b1100_0000 | (limit >> 16)
+    db (base >> 24) & 0xff
+gdt_end:
+
+;检测到的可使用的内存块的个数
+ards_count: 
     dw 0
+;内存检测缓存结构体
 ards_buffer:
+
